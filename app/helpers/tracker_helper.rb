e6aa77bd0bc35d1d1e7aa0d8cfbe50d5096e790b
Max Gordon <gordonmaxc@gmail.com>
2018-05-30 09:05:09 -0700

    Added url info

--- app/helpers/tracker_helper.rb
+++ app/helpers/tracker_helper.rb
@@ -1,6 +1,6 @@
 module TrackerHelper
   def tracker_link_text(tracker_uuid)
-    "<img height='100' width='100' alt='' src='http://#{request.domain}/trackers/#{tracker_uuid}/mark'/>"
+    "<img height='100' width='100' alt='' src='#{ENV['HOSTED_URL']}/trackers/#{tracker_uuid}/mark'/>"
   end
 
   def tracker_link_img(tracker_uuid)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => redirect('/trackers')
+
   resources :trackers, param: :uuid
   get 'trackers/:uuid/mark' => 'trackers#mark'
 end
